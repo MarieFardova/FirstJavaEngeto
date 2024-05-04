26e9a8e93ded8def1357917400ff4ce76ac76d7f
Marie77 <marie.fardova@post.cz>
2024-05-04 19:41:02 +0200

    "excercise1"

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -16,15 +18,34 @@ public class Main {
         double c;
         c = calculateDiagonal(5,6);
         System.out.println("Úhlopříčka trojúhelníku je:  " + c);
+
+/*--------------------------------------------------------------------------------*/
+        String name = "Marie Fardova";
+        LocalDate date = LocalDate.now();
+        date = LocalDate.of(2024, 05, 03);
+        System.out.println(date);
+/*--------------------------------------------------------------------------------
+        double length = 5.3;
+        int lengthInt = (int) length;
+// Desetinné číslo 5.3 se převede na celé tak, že se zahodí desetinná část.
+// V proměnné bude hodnota 5.
+        float width = 10.4f; // Požadujeme hodnotu typu float.
+// Převod čísel na text lze provést:
+        String lengthAsText1 = String.valueOf(length);
+        String lengthAsText2 = ""+length;
+
+     ----------------------------------------------------------------------
+        int length = 5;
+        double lengthDecimal = length; // Automaticky se převede na desetinné číslo.
+        double width = 10; // I tady se provádí konverze z celého čísla na desetinné.
+        ----------------------------------------------------------------------*/
     }
 
 
     private static void printNumbers1To5() {
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
     }
 
 
